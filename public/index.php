6bf1c0036de06d499e79caa1797c2d99235a459d
Matheus Teixeira <matheustalves@outlook.com>
2022-02-21 22:02:28 -0300

    added db

--- public/index.php
+++ public/index.php
@@ -17,6 +17,18 @@ $app = new \Slim\App(['settings' => $config]);
 
 $container = $app->getContainer();
 
+$container['db'] = function ($c) {
+    $db = $c['settings']['db'];
+    $pdo = new PDO(
+        'mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'],
+        $db['user'],
+        $db['pass']
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
+
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
     $response->getBody()->write("Hello, $name");
